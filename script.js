6bc84abff2b99ec87102dc8198dc9b9ff94cc60e
Diogo N. Abreu <134980133+diogooabreu@users.noreply.github.com>
2026-03-25 12:48:36 -0300

    Finish java-script (#3)
    * Add images in the page
    
    * Basical styles: nav-menu
    
    * Main and nav styles
    
    * Add fonts in to project
    
    * Add fonts in to project-2
    
    * Session two created
    
    * Created session three
    
    * Style three session
    
    * Fixed: style.css - session3
    
    * Fix: Style in session three
    
    * doc(updt): README.md
    
    * animation session-two
    
    * Finish

---
+++ script.js
@@ -0,0 +1,129 @@
+gsap.registerPlugin(ScrollTrigger);
+// Crieate timeline with GSAP
+let timeLine = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.two',
+        start: '0%, 95%',
+        end: '70% 50%',
+        scrub: true,
+        makers: false, //depuração
+    },
+});
+
+timeLine.to(
+    '#fanta',
+    {
+        top: '120%',
+        left: '0%',
+    },
+    'orange' //nomeando esse trecho da animação como "orange" para melhorar a sincronização
+);
+
+timeLine.to(
+    '#cut-orange',
+    {
+        top: '155%',
+        left: '23%',
+    },
+    'orange'
+); //sincronizando com a animação orange
+
+timeLine.to(
+    '#orange',
+    {
+        width: '15%',
+        top: '160%',
+        right: '10%',
+    },
+    'orange',
+);
+
+timeLine.to(
+    '#leaf',
+    {
+        top: '110%',
+        rotate: '530deg',
+        left: '70%',
+    },
+    'orange',
+);
+
+timeLine.to(
+    '#leaf-2',
+    {
+        top: '110%',
+        rotate: '530deg',
+        left: '0%',
+    },
+    'orange',
+);
+
+let timeLine2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.three',
+        start: '0% 95%',
+        end: '20% 50%',
+        scrub: true,
+        markers: false,
+    },
+});
+
+timeLine2.from(
+    '.lemon',
+    {
+        rotate: '-90deg',
+        left: '-100%',
+        top: '110%'
+    },
+    'session-tree',
+);
+
+timeLine2.from(
+    '#cocacola',
+    {
+        rotate: '-90deg',
+        left: '-100%',
+        top: '110%'
+    },
+    'session-tree',
+);
+
+timeLine2.from(
+    '.lemon-2',
+    {
+        rotate: '90deg',
+        left: '100%',
+        top: '110%'
+    },
+    'session-tree',
+);
+
+timeLine2.from(
+    '#pepsi',
+    {
+        rotate: '90deg',
+        left: '100%',
+        top: '110%'
+    },
+    'session-tree',
+);
+
+timeLine2.to(
+    '#cut-orange',
+    {
+        width: '16%',
+        left: '42%',
+        top: '204%',
+    },
+    'session-three',
+);
+
+timeLine2.to(
+    '#fanta',
+    {
+        width: '30%',
+        left: '35%',
+        top: '210%',
+    },
+    'session-three',
+);
